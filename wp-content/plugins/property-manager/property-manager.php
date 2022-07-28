d3d99fccdf2494a3e4741b9de246edb0104d28f3
JPG <jpg.soporte@gmail.com>
2022-07-28 09:48:36 -0400

    Adding property manager plugin boilerplate!

---
+++ wp-content/plugins/property-manager/property-manager.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @package Property_Manager
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Property Manager
+Plugin URI: http://google.com/
+Description: Ptoperty manager description
+Author: JPG
+Version: 1.0.0
+Author URI: http://github.com/juanpgarciac
+*/
