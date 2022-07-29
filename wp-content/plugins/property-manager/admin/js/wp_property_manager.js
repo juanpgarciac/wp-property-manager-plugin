d6e727560b10f9edfd69f6725b99749a33cc1606
JPG <jpg.soporte@gmail.com>
2022-07-29 10:01:40 -0400

    Fix location selector, you cannot select more than one location for a property (only hierarchy)

--- wp-content/plugins/property-manager/admin/js/wp_property_manager.js
+++ wp-content/plugins/property-manager/admin/js/wp_property_manager.js
@@ -1,9 +1,9 @@
 
 jQuery(document).on( 'ready', function($){
     $ = jQuery;
-	$('.categorychecklist').on( 'click', 'input[type="checkbox"]', function(){
+	$('#taxonomy-property-cpt-location .categorychecklist').on( 'click', 'input[type="checkbox"]', function(){
+		$('#taxonomy-property-cpt-location .categorychecklist').find( 'input[type="checkbox"]' ).not($(this)).prop('checked',false)
 		if ( ! $(this).is( ':checked' ) ) return;
-
 		$(this).closest( 'ul.children' )
 				.parentsUntil( '.categorychecklist', 'li' )
 				.children( 'label' )
